9c43606d50f634c0a8380ab1ea3b25671902c507
kyle beliveau <kyle.beliveau@techhigh.us>
2014-09-19 09:04:59 -0400

    added decompistion

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,24 +1,33 @@
 public class DrawFigures1 {
     public static void main(String[] args) {
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
+		
+	uptriangle();
+	downtriangle();
+		/*this 
+		is 
+		a comment.*/
         System.out.println();
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
+       
+       uptriangle();
         System.out.println();
 		//comments have been put here.
-        System.out.println("   /\\");
+     uptriangle();
+	StarsStripes();
+     uptriangle();
+    }
+	
+	public static void uptriangle() {
+		 System.out.println("  /\\");
         System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println("+------+");
+        System.out.println(" /    \\")
+	}
+	public static void downtriangle() {
+	    System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+	}
+	public static void StarsStripes() {
+	    System.out.println("+------+");
         System.out.println("|      |");
         System.out.println("|      |");
         System.out.println("+------+");
@@ -28,8 +37,5 @@ public class DrawFigures1 {
         System.out.println("|      |");
         System.out.println("|      |");
         System.out.println("+------+");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-    }
+}
 }
